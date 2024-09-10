e98627473a97f01611f9d5bc318770889be94dc3
mazeraz <116453308+mazeraz@users.noreply.github.com>
2024-09-10 15:09:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -18,3 +18,54 @@ showDiv2Button.addEventListener('click', function() {
     showDiv2Button.classList.add('selected');
     showDiv1Button.classList.remove('selected');
 });
+
+ // change the color of anchor tags based on the currently visible section while scrolling
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+});
+
+
+ // tools change color anchor tags at #tools
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    function changeLinkColor() {
+        let section3Visible = false;
+
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+
+            if (isVisible && section.id === 'section3') {
+                section3Visible = true;
+            }
+        });
+
+        navLinks.forEach((link) => {
+            if (section3Visible) {
+                link.classList.add('section3-visible');
+                link.classList.remove('section-visible');
+            } else {
+                link.classList.remove('section3-visible');
+                link.classList.add('section-visible');
+            }
+        });
+    }
+
+    changeLinkColor();
+    window.addEventListener('scroll', changeLinkColor);
+});
